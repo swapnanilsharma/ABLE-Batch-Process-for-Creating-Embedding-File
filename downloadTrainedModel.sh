86858b4f02f547f13300ba7b3dbf4ce5e95f12db
root <root@jenkins.lopyq3ux3cie1cxn5pu5423osh.ix.internal.cloudapp.net>
2019-11-06 13:16:44 +0000

    modified

--- downloadTrainedModel.sh
+++ downloadTrainedModel.sh
@@ -5,4 +5,3 @@ mkdir -p useModel
 wget 'https://tfhub.dev/google/universal-sentence-encoder-large/3?tf-hub-format=compressed' -O useModel/temp
 tar -zxvf useModel/temp -C useModel/
 rm -rf useModel/temp
-
